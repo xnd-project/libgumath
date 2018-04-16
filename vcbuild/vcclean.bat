eb98334b7173fdf8c1f4086a527376cce65be693
Stefan Krah <skrah@bytereef.org>
2018-04-16 18:54:22 +0200

    More path fixes.

--- vcbuild/vcclean.bat
+++ vcbuild/vcclean.bat
@@ -6,7 +6,7 @@ if exist Makefile nmake /nologo clean
 cd tests
 if exist Makefile nmake /nologo clean
 
-cd ..\..\vcbuild
+cd ..\vcbuild
 if exist dist64 rd /q /s dist64
 if exist dist32 rd /q /s dist32
 
