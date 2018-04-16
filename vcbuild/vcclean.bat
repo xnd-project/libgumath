1d7932228d1cc54b23fa5d35895fbd344b6e88e0
Stefan Krah <skrah@bytereef.org>
2018-04-16 18:57:10 +0200

    More Windows paths.

--- vcbuild/vcclean.bat
+++ vcbuild/vcclean.bat
@@ -3,9 +3,6 @@
 cd ..\libgumath
 if exist Makefile nmake /nologo clean
 
-cd tests
-if exist Makefile nmake /nologo clean
-
 cd ..\vcbuild
 if exist dist64 rd /q /s dist64
 if exist dist32 rd /q /s dist32
--- vcbuild/vcdistclean.bat
+++ vcbuild/vcdistclean.bat
@@ -3,9 +3,6 @@
 cd ..\libgumath
 if exist Makefile nmake /nologo distclean
 
-cd tests
-if exist Makefile nmake /nologo distclean
-
 cd ..\vcbuild
 if exist dist64 rd /q /s dist64
 if exist dist32 rd /q /s dist32
