0bb45d5f88ed714d576a51f408eceed808d027e9
Stefan Krah <skrah@bytereef.org>
2018-05-07 19:57:27 +0200

    Fix file name.

--- .conda/libgumath/bld.bat
+++ .conda/libgumath/bld.bat
@@ -3,4 +3,4 @@ set "LIBXNDINCLUDE=%PREFIX%\Library\include"
 cd "%RECIPE_DIR%\..\..\vcbuild" || exit 1
 call vcbuild64.bat || exit 1
 copy /y dist64\lib* "%PREFIX%\Library\bin\"
-copy /y dist64\xnd.h "%PREFIX%\Library\include\"
+copy /y dist64\gumath.h "%PREFIX%\Library\include\"
