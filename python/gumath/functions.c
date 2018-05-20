8716b0d920daee0d76c63a76e36c0c5e3857f727
Stefan Krah <skrah@bytereef.org>
2018-05-20 19:34:37 +0200

    Add python/gumath/functions.c.
      - Mercurial really got some things right ...

---
+++ python/gumath/functions.c
@@ -0,0 +1,77 @@
+#include <Python.h>
+#include "ndtypes.h"
+#include "pyndtypes.h"
+#include "gumath.h"
+#include "pygumath.h"
+
+
+/****************************************************************************/
+/*                              Module globals                              */
+/****************************************************************************/
+
+/* Function table */
+static gm_tbl_t *table = NULL;
+
+
+/****************************************************************************/
+/*                                  Module                                  */
+/****************************************************************************/
+
+static struct PyModuleDef functions_module = {
+    PyModuleDef_HEAD_INIT,        /* m_base */
+    "functions",                  /* m_name */
+    NULL,                         /* m_doc */
+    -1,                           /* m_size */
+    NULL,                         /* m_methods */
+    NULL,                         /* m_slots */
+    NULL,                         /* m_traverse */
+    NULL,                         /* m_clear */
+    NULL                          /* m_free */
+};
+
+
+PyMODINIT_FUNC
+PyInit_functions(void)
+{
+    NDT_STATIC_CONTEXT(ctx);
+    PyObject *m = NULL;
+    static int initialized = 0;
+
+    if (!initialized) {
+       if (import_ndtypes() < 0) {
+            return NULL;
+       }
+       if (import_gumath() < 0) {
+            return NULL;
+       }
+
+       table = gm_tbl_new(&ctx);
+       if (table == NULL) {
+           return Ndt_SetError(&ctx);
+       }
+
+       if (gm_init_unary_kernels(table, &ctx) < 0) {
+           return Ndt_SetError(&ctx);
+       }
+       if (gm_init_binary_kernels(table, &ctx) < 0) {
+           return Ndt_SetError(&ctx);
+       }
+
+       initialized = 1;
+    }
+
+    m = PyModule_Create(&functions_module);
+    if (m == NULL) {
+        goto error;
+    }
+
+    if (Gumath_AddFunctions(m, table) < 0) {
+        goto error;
+    }
+
+    return m;
+
+error:
+    Py_CLEAR(m);
+    return NULL;
+}
