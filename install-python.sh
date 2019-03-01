a4044d672edd11713769da829c855a5c9b6ee996
Hameer Abbasi <hameerabbasi@yahoo.com>
2019-03-01 17:50:35 +0100

    Move to git submodules

---
+++ install-python.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ -d ndtypes ]] ; then
+  cd ndtypes
+  python3 setup.py install --local=$PWD/../python
+else
+  echo "Did you do git submodule init?"
+  exit 1
+fi
+cd ..
+
+if [[ -d xnd ]] ; then
+  cd xnd
+  python3 setup.py install --local=$PWD/../python
+else
+  echo "Did you do git submodule init?"
+  exit 1
+fi
+cd ..
+
+python3 setup.py build
+
