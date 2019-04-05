6fc03c449e5d09c407b161eb8c824c82785ed8c1
Stefan Krah <skrah@bytereef.org>
2019-04-05 12:42:31 +0200

    Make an explicit list of unary ufuncs.

--- python/test_xndarray.py
+++ python/test_xndarray.py
@@ -127,40 +127,51 @@ class TestOperators(unittest.TestCase):
             self.assertEqual(z.tolist(), c.tolist())
 
 
-
 @unittest.skipIf(np is None, "test requires numpy")
 class TestArrayUfunc(unittest.TestCase):
 
-    ufuncs =[v for v in np.__dict__.values() if isinstance(v, np.ufunc)]
+    unary = [ 'absolute', 'arccos', 'arccosh', 'arcsin', 'arcsinh', 'arctan',
+              'arctanh', 'cbrt', 'ceil', 'conjugate', 'cos', 'cosh', 'degrees',
+              'exp', 'expm1', 'fabs', 'floor', 'invert', 'isfinite', 'isinf',
+              'isnan', 'isnat', 'log', 'log10', 'log1p', 'log2', 'negative',
+              'positive', 'rad2deg', 'radians', 'reciprocal', 'rint', 'sign',
+              'signbit', 'sin', 'sinh', 'spacing', 'sqrt', 'square', 'tan',
+              'tanh', 'trunc', 'absolute' ]
+
+    def test_unary(self):
+        for name in self.unary:
+            f = getattr(np, name)
+            for lst in gen_fixed(3, 1, 5):
+                a = np.array(lst, dtype="float32")
+
+                np_exc = None
+                try:
+                    b = f(a)
+                except Exception as e:
+                    np_exc = e.__class__
+
+                x = array(lst, dtype="float32")
+
+                xnd_exc = None
+                try:
+                    y = f(x)
+                except Exception as e:
+                    xnd_exc = e.__class__
+
+                if np_exc or xnd_exc:
+                    print(f, lst)
+                    self.assertEqual(xnd_exc, np_exc)
+                    continue
+
+                np.testing.assert_array_equal(y, b)
+
+
+@unittest.skipIf(np is None, "test requires numpy")
+class TestArrayFunc(unittest.TestCase):
+
     # funcs = [v for v in np.__dict__.values() if callable(v) and hasattr(v, '__wrapped__')]
-    binary_plus_axis = { "tensordot": (np.ndarray, np.ndarray, int), }
+    binary_plus_axis = { 'tensordot': (np.ndarray, np.ndarray, int), }
     binary = { "dot": (np.ndarray, np.ndarray), }
-
-    def test_ufuncs(self):
-
-        for x in gen_fixed(3, 1, 5):
-            for y in gen_fixed(3, 1, 5):
-                xnd_x = array(x, dtype="float32")
-                xnd_y = array(y, dtype="float32")
-                np_x = np.array(x, dtype="float32")
-                np_y = np.array(y, dtype="float32")
-                for f in self.ufuncs:
-                    arity = 1
-                    try:
-                        a = f(np_x)
-                    except:
-                        arity = 2
-                        try:
-                            a = f(np_x, np_y)
-                        except:
-                            continue
-
-                    if arity == 1:
-                        b = f(xnd_x)
-                    else:
-                        b = f(xnd_x, xnd_y)
-
-                    np.testing.assert_equal(a, b)
 
     @unittest.skipIf(not HAVE_ARRAY_FUNCTION,
                      "test requires numpy with __array_function__ support")
@@ -300,7 +311,7 @@ class TestArrayUfunc(unittest.TestCase):
 
     @unittest.skipIf(not HAVE_ARRAY_FUNCTION,
                      "test requires numpy with __array_function__ support")
-    def test_array_funcs(self):
+    def test_binary(self):
 
         for name in self.binary:
             f = getattr(np, name)
@@ -365,6 +376,7 @@ class TestArrayUfunc(unittest.TestCase):
 ALL_TESTS = [
   TestOperators,
   TestArrayUfunc,
+  TestArrayFunc,
 ]
 
 
